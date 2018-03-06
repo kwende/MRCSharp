3c198cf6fca867eaa75ecaba5658b606db71c8c9
Ben <ben@ben-rush.net>
2018-03-05 23:11:02 -0600

    Random doesn't work so well.

--- MRCSharp/ImageSimulator/Program.cs
+++ MRCSharp/ImageSimulator/Program.cs
@@ -13,8 +13,7 @@ namespace ImageSimulator
         {
             int counter = 0;
             Random rand = new Random(1234);
-            //for (int c = 0; c < 1000; c++)
-            Parallel.For(0, 1000, c =>
+            for (int c = 0; c < 1000; c++)
             {
                 lock (typeof(string))
                 {
@@ -25,7 +24,7 @@ namespace ImageSimulator
                 Tomogram tom = TomogramBuilder.BuildTomogram(860, 934, 100000, rand.Next(5, 25));
                 TomogramBuilder.SaveAsBitmap(tom, $"C:/users/ben/desktop/toms/{c}.bmp");
                 TomogramBuilder.SaveAsDatFile(tom, $"C:/users/ben/desktop/toms/{c}.dat");
-            });
+            }
         }
     }
 }
